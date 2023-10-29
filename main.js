ac60f3fb20f7c677880852feeb86b76f7f98bea4
Akash Shrestha <aakashtha49@gmail.com>
2023-10-28 23:49:31 -0400

    lab 3 firebase done

--- main.js
+++ main.js
@@ -3,9 +3,6 @@ const artistInput = document.getElementById("artist");
 const btnAdd = document.getElementById("btnAdd");
 var formSavedSection = document.getElementsByClassName("form-saved-section");
 
-const songs = [];
-const songsArray = [];
-
 // Service Worker Registration Code Start //
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker
@@ -35,30 +32,101 @@ btnAdd.addEventListener("click", function (event) {
     artistInput.classList.add("shake");
     artistInput.placeholder = "Artist is required";
   } else {
-    console.log("Test111");
-    let title = nameInput.value.trim();
-    let artist = artistInput.value.trim();
-
-    const songDiv = document.createElement("div");
-    songDiv.classList.add("songs-list-section");
-    const titleLbl = document.createElement("h1");
-    titleLbl.textContent = title;
-    const artistLbl = document.createElement("h3");
-    artistLbl.textContent = artist;
-
-    songs.push({ title, artist });
-
-    songs.forEach(function (item) {
-      console.log(item.title);
-      console.log(item.artist);
-
-      songDiv.appendChild(titleLbl);
-      songDiv.appendChild(artistLbl);
-
-      formSavedSection[0].appendChild(songDiv);
-    });
+    saveSongs(nameInput.value.trim(), artistInput.value.trim());
   }
 });
+
+function addToDOM(titleValue, artistValue, likesValue, KeyValue) {
+  let title = titleValue;
+  let artist = artistValue;
+
+  console.log("loop ran");
+
+  const songDiv = document.createElement("div");
+  songDiv.classList.add("songs-list-section");
+
+  const titleLbl = document.createElement("h1");
+  titleLbl.textContent = title;
+  const artistLbl = document.createElement("h3");
+  artistLbl.textContent = artist;
+
+  const titleDiv = document.createElement("div");
+  titleDiv.classList.add("song-title-section");
+  const btnRemove = document.createElement("button");
+  btnRemove.classList.add("btn-remove");
+  btnRemove.innerText = "Remove";
+
+  const likeDiv = document.createElement("div");
+  likeDiv.classList.add("song-like-section");
+  const likeCount = document.createElement("h1");
+  likeCount.textContent = "Likes: " + likesValue;
+  const btnLike = document.createElement("button");
+  btnLike.classList.add("btn-like");
+  btnLike.innerText = "👍 Like";
+
+  titleDiv.appendChild(titleLbl);
+  titleDiv.appendChild(artistLbl);
+  titleDiv.appendChild(btnRemove);
+
+  likeDiv.appendChild(likeCount);
+  likeDiv.appendChild(btnLike);
+
+  songDiv.appendChild(titleDiv);
+  songDiv.appendChild(likeDiv);
+
+  btnRemove.addEventListener("click", () => removeSong(KeyValue));
+  btnLike.addEventListener("click", () => increaseLike(likesValue, KeyValue));
+
+  formSavedSection[0].appendChild(songDiv);
+}
+
+function getSongs() {
+  firebase
+    .database()
+    .ref("akash/songs")
+    .on("value", (snapshot) => {
+      if (snapshot.exists()) {
+        formSavedSection[0].innerHTML =
+          '<div class="title-section"> <h2>LIST OF SONGS</h2></div>';
+        snapshot.forEach((item) => {
+          const songItem = item.val();
+          addToDOM(songItem.title, songItem.artist, songItem.likes, item.key);
+        });
+      } else {
+        formSavedSection[0].innerHTML =
+          '<div class="title-section"> <h2>LIST OF SONGS</h2></div>';
+      }
+    });
+}
+
+function saveSongs(titleValue, artistValue) {
+  const dbRef = firebase.database().ref("akash/songs").push();
+  dbRef.set(
+    {
+      title: titleValue,
+      artist: artistValue,
+      likes: 0,
+    },
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+}
+
+function increaseLike(likesValue, KeyValue) {
+  firebase
+    .database()
+    .ref(`akash/songs/${KeyValue}`)
+    .update({
+      likes: Number(likesValue) + 1,
+    });
+}
+
+function removeSong(KeyValue) {
+  firebase.database().ref(`akash/songs/${KeyValue}`).remove();
+}
 
 nameInput.addEventListener("focus", function () {
   nameInput.value = "";
@@ -67,3 +135,5 @@ nameInput.addEventListener("focus", function () {
 artistInput.addEventListener("focus", function () {
   artistInput.value = "";
 });
+
+getSongs();
